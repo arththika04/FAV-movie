bb0ffeae9e0ff0d176d3d0db5b5f8209a437bf1b
Aarthika <aarthika@gmail.com>
2026-01-07 15:41:02 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+}
